d182bea62f9a0372eec19fc9253d1d6a2a9e6239
RenaWevin <renawevin@gmail.com>
2023-10-18 22:08:02 +0800

    [+] 建立水果相關物件腳本(尚未撰寫完畢)

---
+++ Assets/Scripts/Game/FruitFactory.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitFactory : MonoBehaviour {
+
+    #region 資源區
+
+    /// <summary>
+    /// 水果生成來源
+    /// </summary>
+    [SerializeField]
+    private FruitObject fruitPrefab_Original;
+
+    /// <summary>
+    /// 水果物件池
+    /// </summary>
+    private Queue<FruitObject> objPool_Fruits;
+
+    #endregion
+    #region 初始化
+
+
+
+    #endregion
+    #region 外部-生成水果
+
+
+    public void SpawnFruit() {
+
+    }
+
+    #endregion
+    #region 外部-回收水果
+
+    /// <summary>
+    /// 回收水果
+    /// </summary>
+    public void DisposeFruit() {
+
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/Game/FruitObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitObject : MonoBehaviour {
+
+    #region enum
+
+    /// <summary>
+    /// 水果種類
+    /// </summary>
+    public enum FruitType {
+
+    }
+
+    #endregion
+
+
+
+}
