e151bb039a7e47cb6b4817627ee6f7794d64d3b8
RenaWevin <renawevin@gmail.com>
2023-10-20 17:23:59 +0800

    [+] 新增可遮斷的Log方法

---
+++ Assets/Scripts/Global/Log.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public static class Log {
+
+    private const bool IS_ALLOW_LOG = true;
+
+    public static void Info(object message) {
+        if (IS_ALLOW_LOG) {
+            Debug.Log(message);
+        }
+    }
+    
+    public static void Warning(object message) {
+        if (IS_ALLOW_LOG) {
+            Debug.LogWarning(message);
+        }
+    }
+    
+    public static void Error(object message) {
+        if (IS_ALLOW_LOG) {
+            Debug.LogError(message);
+        }
+    }
+
+}
