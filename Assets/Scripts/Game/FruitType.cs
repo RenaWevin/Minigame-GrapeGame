7a1192d1bd1fac8a500ee37272464b141b02c5e8
RenaWevin <renawevin@gmail.com>
2023-10-25 20:27:49 +0800

    [+] 新增種子與單顆葡萄的階段、水果物件內容更改

--- Assets/Scripts/Game/FruitType.cs
+++ Assets/Scripts/Game/FruitType.cs
@@ -3,16 +3,18 @@
 /// 水果種類
 /// </summary>
 public enum FruitType {
-    Grape = 1,
-    Apple = 2,
-    Pineapple = 3,
-    Papaya = 4,
-    Peach = 5,
-    Watermelon = 6,
-    HealingLuka_Uhhuh = 7,
+    Seed = 1,
+    PieceOfGrape = 2,
+    Grape = 3,
+    Apple = 4,
+    Pineapple = 5,
+    Papaya = 6,
+    Peach = 7,
+    Watermelon = 8,
+    HealingLuka_Uhhuh = 9,
 
     /// <summary>此階段的水果會消失</summary>
-    Disappear = 8,
+    Disappear = 10,
 
     /// <summary>此種類的水果是一種鬼牌，可以和任何水果結合</summary>
     Joker = -270
