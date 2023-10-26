b11919e22b4913c7ca343c2802228def76304890
RenaWevin <renawevin@gmail.com>
2023-10-26 22:56:44 +0800

    [+][!] 更換箭頭材質與設定、新增進化之路UI物件

---
+++ Assets/Scripts/Game/FruitEvolutionListObject.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FruitEvolutionListObject : MonoBehaviour {
+
+    [SerializeField]
+    private Image image_Fruit;
+
+    [SerializeField]
+    private Text text_FruitName;
+
+    public void SetSprite(Sprite sprite) {
+        if (image_Fruit != null) {
+            image_Fruit.sprite = sprite;
+        }
+    }
+
+    public void SetFruitName(string name) {
+        if (text_FruitName != null) {
+            text_FruitName.text = name;
+        }
+    }
+
+}
