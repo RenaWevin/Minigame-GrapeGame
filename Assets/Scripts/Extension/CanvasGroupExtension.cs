896e670ffbc2249fb838e1b745486491d7fa9b7e
RenaWevin <renawevin@gmail.com>
2023-10-27 00:03:14 +0800

    [+] 新增CanvasGroupExtension

---
+++ Assets/Scripts/Extension/CanvasGroupExtension.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public static class CanvasGroupExtension {
+
+    public static void SetEnable(this CanvasGroup canvasGroup, bool enabled) {
+        canvasGroup.alpha = enabled ? 1 : 0;
+        canvasGroup.interactable = enabled;
+        canvasGroup.blocksRaycasts = enabled;
+    }
+
+}
