4c816efa36ff142602d8af2e4f0e3ac3d4751e2c
RenaWevin <renawevin@gmail.com>
2023-11-06 01:30:02 +0800

    [+] 存檔相關空Helper建立

---
+++ Assets/Scripts/Global/Helper/LeaderboardDataHelper.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LeaderboardDataHelper {
+
+    ///// <summary>
+    ///// 記錄新分數
+    ///// </summary>
+    //public static void SaveNewRecord(int newScore) {
+    //
+    //}
+
+}
---
+++ Assets/Scripts/Global/Helper/PlayerPrefHelper.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerPrefHelper {
+
+    #region 設定相關
+
+    
+
+    #endregion
+
+}
