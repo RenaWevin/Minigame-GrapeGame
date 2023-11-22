45f5561e375fc7a9987356b98a86695fe759e4e9
RenaWevin <renawevin@gmail.com>
2023-11-23 00:45:14 +0800

    [+] 實裝首頁高分紀錄頁面的顯示分數資訊

--- Assets/Scripts/Page/LeaderBoardPage.cs
+++ Assets/Scripts/Page/LeaderBoardPage.cs
@@ -12,6 +12,13 @@ public class LeaderBoardPage : MonoBehaviour {
     [SerializeField]
     private Button Button_Close;
 
+    #region  -> 排行榜物件
+
+    [SerializeField] private Text[] Text_Scores;
+    [SerializeField] private Text[] Text_Names;
+
+    #endregion
+
     #endregion
     #region Awake
 
@@ -25,13 +32,43 @@ public class LeaderBoardPage : MonoBehaviour {
     #endregion
     #region UI事件
 
+    #region  -> 顯示/關閉視窗
+
     /// <summary>
     /// 顯示/關閉視窗
     /// </summary>
     /// <param name="show"></param>
     public void SetShowWindow(bool show) {
         canvasGroupWindow.SetShowWindow(show);
+        if (show) {
+            UpdateDisplay_Leaderboard();
+        }
     }
+
+    #endregion
+    #region  -> 刷新顯示分數
+
+    /// <summary>
+    /// 刷新顯示分數
+    /// </summary>
+    private void UpdateDisplay_Leaderboard() {
+        LeaderboardDataComponent leaderboardDataComponent = Core.Instance.leaderboardDataComponent;
+        for (int i = LeaderboardDataComponent.FirstRank; i <= LeaderboardDataComponent.LastRank; i++) {
+            Text textName = Text_Names[i - 1];
+            Text textScore = Text_Scores[i - 1];
+            if (leaderboardDataComponent.TryGetLeaderboardData(i, out var data)) {
+                textName.fontStyle = FontStyle.Normal;
+                textName.text = data.Name;
+                textScore.text = data.Score.ToString();
+            } else {
+                textName.fontStyle = FontStyle.Italic;
+                textName.text = "<無紀錄>";
+                textScore.text = "------";
+            }
+        }
+    }
+
+    #endregion
 
     #endregion
 
