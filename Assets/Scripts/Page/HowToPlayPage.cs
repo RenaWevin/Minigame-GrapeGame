41aae37c550f9edbc323c14eebd4df40fab2c6ff
RenaWevin <renawevin@gmail.com>
2023-11-28 20:28:10 +0800

    [+] 實裝遊玩方法畫面的操作方式文字解說

--- Assets/Scripts/Page/HowToPlayPage.cs
+++ Assets/Scripts/Page/HowToPlayPage.cs
@@ -12,6 +12,13 @@ public class HowToPlayPage : MonoBehaviour {
     [SerializeField]
     private Button Button_Close;
 
+    #region  -> 控制方法
+
+    [SerializeField]
+    private Text Text_Controll_HowToPlay;
+
+    #endregion
+
     #endregion
     #region Awake
 
@@ -31,6 +38,21 @@ public class HowToPlayPage : MonoBehaviour {
     /// <param name="show"></param>
     public void SetShowWindow(bool show) {
         canvasGroupWindow.SetShowWindow(show);
+        if (show) {
+            UpdateDisplayPage();
+        }
+    }
+
+    /// <summary>
+    /// 更新頁面
+    /// </summary>
+    private void UpdateDisplayPage() {
+        //操作方法
+        OptionsPage optionsPage = Core.Instance.optionsPage;
+        string leftkey = Core.Instance.keyCodeNameComponent.GetKeyCodeName(optionsPage.keycode_MoveLeft);
+        string rightkey = Core.Instance.keyCodeNameComponent.GetKeyCodeName(optionsPage.keycode_MoveRight);
+        string putFruit = Core.Instance.keyCodeNameComponent.GetKeyCodeName(optionsPage.keycode_PutFruit);
+        Text_Controll_HowToPlay.text = $"使用 {leftkey} 與 {rightkey} 左右移動\n按 {putFruit} 放下水果";
     }
 
     #endregion
